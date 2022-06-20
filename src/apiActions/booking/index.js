9eb63ad05daf5c6b0d4b2d1dc13c25621fb3018d
Harshal <harshal@datacube.ae>
2022-06-20 12:34:25 +0530

    removed old code

--- src/apiActions/booking/index.js
+++
@@ -1,9 +0,0 @@
-import CallAPi from 'src/Utils/axios';
-import apiUrls from 'src/Utils/apiUrls';
-
-export const updateBooking = (id, body) => {
-  return CallAPi.post(`${apiUrls.updateBooking}/${id}`, body)
-}
-export const deleteYatch = (body) => {
-  return CallAPi.post(`${apiUrls.deleteYatch}`, body)
-}
--- src/apolloclient.js
+++
@@ -1,8 +0,0 @@
-import ApolloClient from 'apollo-boost';
-import constant from './constant';
-
-const client = new ApolloClient({
-  uri: `${constant.apiurl}/graphql`
-});
-
-export default client;
